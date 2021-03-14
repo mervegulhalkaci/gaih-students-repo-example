fd1186c53cd4adfb40cff83debce798730f1cdc0
mervegulhalkaci <80275176+mervegulhalkaci@users.noreply.github.com>
2021-03-14 23:36:28 +0300

    Add files via upload

---
+++ ödev1.py
@@ -0,0 +1,12 @@
+list1 = [1,3,5,33,9,11,13,15,17,51,103]               #tek sayılardan oluşan liste.
+list2 = [2,4,6,58,10,12,24,62,18,20,100]              #çift sayılardan oluşan liste.
+
+list = list1+list2                                    #tek sayılardan ve çift sayılardan oluşan listemizden yeni bir liste oluşturduk.
+print(list)                                            #ekranda gösterdik.
+
+liste = [i * 2 for i in list]                          #oluşan listedeki her bir değeri 2 ile çarptık.
+print(liste)                                           #ekranda gösterdik.
+
+
+for i in liste:
+   print(type(i) )                                #hangi tipte olduğunu belirledik.
---
+++ ödev2.py
@@ -0,0 +1,40 @@
+CV = {"cv_1": {"İsim": "Merve Gül Halkacı",
+                     "Memleket": "konya",
+                     "Yaş": "20",
+                     "Öğrenim Durumu": "Lisans",
+                     "Başvurduğu Statü": "Elektrik elektronik müh"},
+
+
+           "cv_2": {"İsim": "Selma Yılmaz" ,
+                               "Memleket": "konya" ,
+                               "Yaş"     : "22" ,
+                               "Öğrenim Durumu": "Lisans" ,
+                               "Başvurduğu Statü": "Elektrik elektronik müh"},
+
+
+           "cv_3": {"İsim": "Arzu Dayı" ,
+                               "Memleket": "istanbul" ,
+                               "Yaş"     : "30" ,
+                               "Öğrenim Durumu": "Lisans" ,
+                               "Başvurduğu Statü": "insan kaynaklrı müdürü"},
+
+           "cv_4": {"İsim": "Deniz Çoban" ,
+                               "Memleket": "Mersin" ,
+                               "Yaş"     : "33" ,
+                               "Öğrenim Durumu": "Lisans" ,
+                               "Başvurduğu Statü": "insan kaynakları müdürü"},
+
+           "cv_5": {"İsim": "Ahmet Demir" ,
+                               "Memleket": "Ankara" ,
+                               "Yaş"     : "28" ,
+                               "Öğrenim Durumu": "Lisans" ,
+                               "Başvurduğu Statü": "Bilgisayar müh."}
+           }
+
+
+print(CV["cv_1"])
+print(CV["cv_2"])
+print(CV["cv_3"])
+print(CV["cv_4"])
+print(CV["cv_5"])
+
